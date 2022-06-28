034703f614c425354491b356e75d6f02d658a76a
emonShimoul <emonshimoul@gmail.com>
2022-06-29 01:25:00 +0600

    add products to database using POST API

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddProduct = () => {
-    return (
+    const nameRef = useRef();
+    const priceRef = useRef();
+    const quantityRef = useRef();
+    const handleSubmit = e => {
+        const name = nameRef.current.value;
+        const price = priceRef.current.value;
+        const quantity = quantityRef.current.value;
+        const productsDetails = {name: name, price: price, quantity: quantity}
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productsDetails)
+        })
+        .then()
+
+
+        nameRef.current.value = "";
+        priceRef.current.value="";
+        quantityRef.current.value="";
+        e.preventDefault();
+    }
+
+    return ( 
         <div>
-            <h2>This is Add User</h2>
+            <h2>Add Product</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" ref={nameRef} placeholder='Name' />
+                <input type="text" ref={priceRef} placeholder='Price' />
+                <input type="number" ref={quantityRef} placeholder='Quantity' />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
