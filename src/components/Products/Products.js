fd9333d053cb4e60b28cc6dad05c923bac76d709
emonShimoul <emonshimoul@gmail.com>
2022-06-29 02:15:15 +0600

    added deleted product functionality

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
+    const[products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    } ,[]);
+
+    const handleDelete = id => {
+        const proceed = window.confirm("Are you sure, you want to delete??");
+        if(proceed){
+            fetch(`http://localhost:5000/products/${id}`, {
+                method: "DELETE"
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount>0){
+                    // console.log(data);
+                    alert("Deleted Successfully!!");
+                    const remainingProducts = products.filter(product => product._id !== id);
+                    setProducts(remainingProducts);
+                }
+            });
+        }
+    } 
+
     return (
         <div>
-            <h2>This is Products</h2>
+            <h2>Products</h2>
+            <ul>
+                {
+                    products.map(product => <li key={product._id}>
+                        {product.name} - {product.price} - {product.quantity} {" "} 
+                        <button onClick={() => handleDelete(product._id)}>X</button> {" "} 
+                        <button>Update</button>
+                        </li> )
+                }
+            </ul>
         </div>
     );
 };
